44bd5ede13edcc5daa5ad439aecd832da073635e
Sergio Esteban <Sergio.Esteban@externals.adidas.com>
2019-02-27 18:52:33 +0100

    Added node configuration

--- src/server.js
+++ src/server.js
@@ -18,25 +18,40 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Some variables 
 var PORT = 3000
+
+// endpoints
+var LOGIN = '/login'
 var INDEX = '/'
+
+// html files
+var LOGIN_HTML = 'login'
 var INDEX_HTML = 'index'
 
 // Enpoints an action for the services
-// GET /
+
+// /login Endpoint
+// GET 
+app.get(LOGIN, function (req, res) {
+    res.render(LOGIN_HTML);
+})
+
+// POST 
+app.post(LOGIN, function (req, res) {
+    // we obtain this from html from tag: name in input
+    //console.log(req.body.city)
+    //let cityTyped = req.body.city;
+    // then we pass the information to the front in a dictionary way (parserbody things)
+    // we have to manage this variables in the frontend 
+    res.render(INDEX_HTML);
+})
+
+// / endpoint
+// GET 
 app.get(INDEX, function (req, res) {
     res.render(INDEX_HTML);
 })
 
-// POST /
-app.post(INDEX, function (req, res) {
-    // we obtain this from html from tag: name in input
-    console.log(req.body.city)
-    let cityTyped = req.body.city;
-    // then we pass the information to the front in a dictionary way (parserbody things)
-    // we have to manage this variables in the frontend 
-    res.render('cityType.ejs', {cityTyped: cityTyped, error: null})
-})
-
+// runner for the server
 app.listen(PORT, function () {
-    console.log('Server listening in: http://localhost:3000')
+    console.log('Server listening in: http://localhost:' + PORT)
 })
